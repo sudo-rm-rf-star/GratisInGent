25081bb7f0be88c213b4621f877a7baebb8425ef
Jarre Knockaert <jarre@edgardcooper.com>
2021-11-05 01:02:40 +0100

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
   // tailwind.config.js
   module.exports = {
-   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
      fontFamily: {
